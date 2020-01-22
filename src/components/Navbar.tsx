5e71ea8459f6dcdbe96578b43ddd049c54989c57
kemenykristof <kemeny.kristof24@gmail.com>
2020-01-22 16:04:03 +0100

    logo styling

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -9,22 +9,30 @@ const Nav = styled.div`
   padding: 6px;
   background-color: #202020;
   font-size: 20px;
-  color: #E1B73B;
+  color: #e1b73b;
   text-align: center;
 `;
-const Text = styled.p`
-  color: "goldenrod";
+const Text = styled.div`
+  padding: 15px 5px 0px 5px;
+  border-radius: 10px;
+  font-weight: bold;
+  background-color: #e1b73b;
+  color: #202020;
 `;
 const Navbar = props => {
   return (
     <Nav>
       <Text>KMDb</Text>
-      <NavLink style={{color: "#E1B73B"}} to="/">Popular movies</NavLink>
+      <NavLink style={{ color: "#E1B73B" }} to="/">
+        Popular movies
+      </NavLink>
       <SearchField
         handleSearch={props.handleSearch}
         handleChange={props.handleChange}
       ></SearchField>
-      <NavLink style={{color: "#E1B73B"}} to="/watchlist">Watchlist</NavLink>
+      <NavLink style={{ color: "#E1B73B" }} to="/watchlist">
+        Watchlist
+      </NavLink>
     </Nav>
   );
 };
