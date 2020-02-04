7b2a1472c3dbb40f82729a7b68b1a02887eaabc5
kemenykristof <kemeny.kristof24@gmail.com>
2020-02-04 16:51:09 +0100

    localstorage for watchlist and  some type error fix

--- src/contexts/WatchlistContext.tsx
+++ src/contexts/WatchlistContext.tsx
@@ -1,4 +1,4 @@
-import React, { createContext, useReducer } from "react";
+import React, { createContext, useReducer, useEffect } from "react";
 import { watchlistReducer } from "../reducers/WatchlistReducer";
 import { Movie } from "../interfaces/types";
 
@@ -7,11 +7,18 @@ type ContextType = {
   dispatch: React.Dispatch<any>;
 };
 
-export const WatchlistContext = createContext<ContextType | any>('');
+export const WatchlistContext = createContext<ContextType | any>("");
 
 const WatchlistContextProvider = (props: { children: React.ReactNode }) => {
-  //@ts-ignore
-  const [watchlist, dispatch] = useReducer(watchlistReducer, []);
+  const [watchlist, dispatch] = useReducer(watchlistReducer, [], ()=>{
+    const localData = localStorage.getItem('watchlist');
+    return localData ? JSON.parse(localData) : [];
+  });
+
+
+  useEffect(() => {
+    localStorage.setItem("watchlist", JSON.stringify(watchlist));
+  }, [watchlist]);
 
   return (
     <WatchlistContext.Provider
